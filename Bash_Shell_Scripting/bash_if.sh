bcf805b9d9d4450e04f9e21e62ab5a72d66c7208
tolulope <ta@anmutetechnologies.com>
2024-04-24 23:15:04 -0500

    Added more information

--- Bash_Shell_Scripting/bash_if.sh
+++ Bash_Shell_Scripting/bash_if.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-MY_SHELL="bash"
+MY_SHELL="tolu"
 
 if [ "$MY_SHELL" = "bash" ]
 then
    echo "You seem to like the bash shell."
+else
+   echo "You don't seem to like the bash shell."
 fi
